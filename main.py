8880f4c03910c2c6303afa8dbde6e63bb8b5b8b5
Gabriel <gabrielcorbot@gmail.com>
2022-09-10 10:36:20 -0500

    Páginas creadas

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ import eel
 import os
 
 path_web = os.path.dirname(os.path.realpath(__file__)) + "\\web"
-
 eel.init(path_web)
 
-@eel.expose
-
-def add(num1, num2):
-    output = int(num1) + int(num2)
-    return output
+# @eel.expose       # en cada def
 
 eel.start("index.html")
--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,13 @@
-document.querySelector("button").onclick = function summation(){  
-    var data_1 = document.getElementById("n1").value  
-    var data_2 = document.getElementById("n2").value  
-    eel.add(data_1, data_2)(call_Back)  
-}  
-function call_Back(output){  
-    document.getElementById("res").value = output  
-}  
+// document.querySelector("button").onclick = function summation(){ 
+    // Args de la función que se va a llamar en python
+    // var data_1 = document.getElementById("n1").value  
+    // var data_2 = document.getElementById("n2").value  
+
+    // LLamado a la función de python
+    // eel._____()(call_Back)  
+// }  
+
+// Función que escribe el resultado del python en el html
+// function call_Back(output){  
+//     document.getElementById("").value = output  
+// }  

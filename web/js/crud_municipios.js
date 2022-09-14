fea3201abf937ef4721d9554a3c4eefa923e534b
Gabriel <gabrielcorbot@gmail.com>
2022-09-14 17:35:30 -0500

    Template para realizar las otras operaciones

---
+++ web/js/crud_municipios.js
@@ -0,0 +1,30 @@
+// CREATE
+// document.querySelector(".crud_create").onclick = function (){ 
+//     console.log('create');
+// }  
+
+// READ
+window.onload = function () {
+    eel.select()(fill_table);
+}
+function fill_table(output){
+    json_list = JSON.parse(output);
+    string = "<tr><th>Id municipio</th><th>Nombre del municipio</th></tr>"
+    json_list.forEach(row => string = string.concat("<tr><td>", row[0], "</td>", "<td>", row[1] ,"</td></tr>"));
+    console.log(string);
+    document.getElementById("data").innerHTML = string;
+}
+
+//UPDATE
+// document.querySelector(".crud_update").onclick = function (){ 
+//     console.log('update');
+// } 
+
+//DELETE
+// document.querySelector(".crud_delete").onclick = function (){ 
+//     console.log('delete');
+// }  
+
+
+
+

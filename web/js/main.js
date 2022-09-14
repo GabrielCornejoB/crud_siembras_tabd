91971e78ed8d769accd873a2e4fd9c8a4c2f2ce3
Gabriel <gabrielcorbot@gmail.com>
2022-09-14 14:12:00 -0500

    toggle de los divs del crud

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,30 @@
-// Toggle active en menu crud
+let div_create = document.getElementById("div_create");
+let div_update = document.getElementById("div_update");
+let div_delete = document.getElementById("div_delete");
+div_create.hidden = false;
+div_update.hidden = true;
+div_delete.hidden = true;
+let current_div = div_create;
+
 const callToActionBtns = document.querySelectorAll(".btn_crud_menu");
 callToActionBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    callToActionBtns.forEach(f => f.classList.remove('active'));
-    e.target.classList.toggle("active");
-  });
+    btn.addEventListener("click", (e) => {
+        callToActionBtns.forEach(f => f.classList.remove('active'));
+        e.target.classList.toggle("active");
+
+        current_div.hidden = true;  
+        if(btn.classList[1] === "create") {
+            current_div = div_create;
+            current_div.hidden = false;
+        }
+        if(btn.classList[1] === "update") {
+            current_div = div_update;
+            current_div.hidden = false;
+        }
+        else if(btn.classList[1] === "delete") {
+            current_div = div_delete;
+            current_div.hidden = false;
+        }
+    });
 });
+

bfe0854c2a8eed4a7e63442befd46340a89a4522
Gabriel <gabrielcorbot@gmail.com>
2022-09-21 23:27:52 -0500

    READ tabla veredas

---
+++ web/js/cruds/crud_veredas.js
@@ -0,0 +1,15 @@
+// CREATE
+
+// READ
+window.onload = function() {
+    eel.select("veredas")(fill_table);
+}
+function fill_table(output) {
+    json_list = JSON.parse(output);
+    string = "<tr><th>Id vereda</th><th>Nombre vereda</th><th>Id municipio</th></tr>";
+    json_list.forEach(row => string = string.concat("<tr><td>", row[0], "</td><td>", row[1], "</td><td>", row[2],"</td></tr>"));
+    document.getElementById("data").innerHTML = string;
+}
+// UPDATE
+
+// DELETE

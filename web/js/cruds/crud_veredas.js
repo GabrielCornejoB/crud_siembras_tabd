663c71c16271bbe834db8bb4ba6b6e6aa8c26f1b
Gabriel <gabrielcorbot@gmail.com>
2022-10-01 09:22:32 -0500

    Campos para crud veredas

--- web/js/cruds/crud_veredas.js
+++ web/js/cruds/crud_veredas.js
@@ -2,13 +2,27 @@
 
 // READ
 window.onload = function() {
-    eel.select("veredas")(fill_table);
+    eel.select("veredas")(get_data);
 }
-function fill_table(output) {
+function get_data(output) {
+    console.log("read");
     json_list = JSON.parse(output);
-    string = "<tr><th>Id vereda</th><th>Nombre vereda</th><th>Id municipio</th></tr>";
-    json_list.forEach(row => string = string.concat("<tr><td>", row[0], "</td><td>", row[1], "</td><td>", row[2],"</td></tr>"));
-    document.getElementById("data").innerHTML = string;
+    string_table = "<tr><th>Id vereda</th><th>Nombre vereda</th><th>Id municipio</th></tr>";
+    json_list.forEach(row => string_table = string_table.concat("<tr><td>", row[0], "</td><td>", row[1], "</td><td>", row[2],"</td></tr>"));
+    document.getElementById("data").innerHTML = string_table;
+    search_muns();
+}
+function search_muns() {
+    console.log("searh_muns");
+    eel.select("municipios")(change_selects);
+}
+function change_selects(output) {
+    console.log("change_selects");
+    json_list = JSON.parse(output);
+    string_select = "<option disables selected value style='color:gray'></option>";
+    json_list.forEach(row => string_select = string_select.concat("<option value='", row[0], "'>", row[0], " - ", row[1], "</option>"));
+    document.getElementById("create_mun").innerHTML = string_select;
+    document.getElementById("update_mun").innerHTML = string_select;
 }
 
 // UPDATE
